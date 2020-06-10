d044e1fb494b2499f7b28ef7560ca3e37e8db6cb
Francisco-11 <informatica.vera2@gmail.com>
2020-06-10 13:15:58 -0400

    utilizacion de yargs

--- 03-Bases-Node/app.js
+++ 03-Bases-Node/app.js
@@ -2,6 +2,20 @@
 // const file = require('fs');
 // const fs = require('express); de libreria externa
 // const fs = require('./fs); ruta de archivos creados por ns
+const argv = require('yargs')
+    .command('listar', 'Imprime en consola la tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        }
+    })
+    .help()
+    .argv;
+
 const { crearArchivo } = require('./multiplicar/multiplicar');
 
 // let base = 'abc';
@@ -10,13 +24,17 @@ const { crearArchivo } = require('./multiplicar/multiplicar');
 // Esto es sumamente volatil porque si no se manda un valor o no se manda el tercer argumento 
 // Ociurre error, y son muchas las validaciones que se deben realizar
 // Para ello se ocupan paquetes q optimizan esto
-let argv = process.argv;
-let parametro = argv[2];
-let base = parametro.split('=')[1];
+let argv2 = process.argv;
+/* let parametro = argv[2];
+let base = parametro.split('=')[1]; */
+
+console.log(argv.base);
+// console.log(argv2);
+console.log('limite', argv.limite);
 
 // console.log(base);
-crearArchivo(base)
+/* crearArchivo(base)
     .then(archivo => console.log(`Archivo creado: ${ archivo}`))
     .catch((err) => {
         console.log(err);
-    })
+    }) */
