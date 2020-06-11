da0d4a1276f6eedb997724ae8c6a07754cff1d4e
Francisco-11 <informatica.vera2@gmail.com>
2020-06-11 17:38:04 -0400

    Lista la seccion para ser subida con heroku

--- 06-Webserver/server.js
+++ 06-Webserver/server.js
@@ -1,21 +1,27 @@
-var express = require('express');
-
+const express = require('express');
 const app = express();
+
+const hbs = require('hbs');
+require('./hbs/helpers');
 
 app.use(express.static(__dirname + '/public'))
 
+// Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+
+app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-    // res.send('Hola Mundo')
-    let salida = {
-        nombre: 'fernando',
-        edad: 32,
-        url: req.url
-    };
 
-    res.send(salida);
+    res.render('home', {
+        nombre: 'Francisco'
+    });
 });
 
+app.get('/about', (req, res) => {
+
+    res.render('about');
+});
 
 app.listen(3000, () => {
     console.log('Escuchando peticiones en el puerto 3000');
